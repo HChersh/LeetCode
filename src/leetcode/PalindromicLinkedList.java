48eef2f2dc9758842a201ed076580596e4386da6
Huangchuan <huangchuan5@yonyou.com>
2017-06-20 16:25:02 +0800

    判断回文链表
    Change-Id: Id313cb25a2f1b684ea93d6d262146cc3d67f63d9
    Signed-off-by: Huangchuan <huangchuan5@yonyou.com>

--- src/leetcode/PalindromicLinkedList.java
+++ src/leetcode/PalindromicLinkedList.java
@@ -11,6 +11,7 @@ public class PalindromicLinkedList { // 判断链表是否为一个回文序列
 		Node left;
 		Node right;
 		Node temp = head;
+		boolean flag = true;
 //        int length = 0;
 //        while(temp != null){      //通过便利链表得到长度来找到中间节点真是垃圾！！！
 //        	temp = temp.next;
@@ -21,17 +22,35 @@ public class PalindromicLinkedList { // 判断链表是否为一个回文序列
            n1 = n1.next;        //中间节点
            n2 = n2.next.next;   //尾部节点
         }
-        
-        
-		return true;
+        right = n1.next;        //指向右侧的第一个节点
+        while(right != null){
+        	if(flag){                         //第一次，将反转后的尾节点的next指向null
+        		n1.next = null;
+        		flag = false;
+        	}
+        	temp = right;
+        	right = right.next;
+        	temp.next = n1;
+        	n1 = temp;
+        }
+        left = head;    //左右头赋值
+        right = n1; 
+        while(left.next != null){
+        	if(left.value != right.value)
+        		return false;
+        	left = left.next;
+        	right = right.next;
+        }
+        return true;
 	}
 
 	public static void main(String[] args) {
-       Node node5 = new Node(1,null);   //1->2->3->2->1
+       Node node5 = new Node(1,null);    //1->2->3->3->2->1
        Node node4 = new Node(2,node5);
        Node node3 = new Node(3,node4);
-       Node node2 = new Node(2,node3);
-       Node node1 = new Node(1,node2);
-       System.out.println(isPalindromel(node1));
+       Node node2 = new Node(3,node3);
+       Node node1 = new Node(2,node2);
+       Node node0 = new Node(1,node1);
+       System.out.println(isPalindromel(node0));
 	}
 }
