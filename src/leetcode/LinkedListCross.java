ba01fb7f8c688edb07f828abc8548dcc81c8b06f
ChuanMac <845825101@qq.com>
2017-06-28 11:33:48 +0800

    mac修改
    Change-Id: I3e4e23fa5cb20ceb3cd399f714c5dbc4fd8ff9ba
    Signed-off-by: ChuanMac <845825101@qq.com>

--- src/leetcode/LinkedListCross.java
+++ src/leetcode/LinkedListCross.java
@@ -1,13 +1,10 @@
 package leetcode;
+
 /**
- * 判断两个链表是否相交，如果相交则返回相交后的第一个节点
- * 问题可以分为一下三个子问题：
+ * 判断两个链表是否相交，如果相交则返回相交后的第一个节点 问题可以分为一下三个子问题： 
  * 1.两个链表是否各自为环，两个链表一个为环，一个不为环，则肯定不相交
- * 2.
- * 
- *
- * 
- * 
+ * 2.两个链表都不为环，判断是否相交 
+ * 3.两个链表都为环，判断是否相交
  * 
  */
 public class LinkedListCross {
@@ -16,7 +13,9 @@ public class LinkedListCross {
 		// TODO Auto-generated method stub
 
 	}
-	
 
+	public boolean isRing(Node head) {
+		return false;
+	}
 
 }
