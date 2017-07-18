cc3f1cb05160f27ee2802ea8b89750758d51cb7e
ChuanMac <845825101@qq.com>
2017-07-18 08:36:35 +0800

    求最大子矩阵的大小 Change-Id: Id2639260b1b25014a15e5b2993bb2d5cfa31684e Signed-off-by: ChuanMac <845825101@qq.com>

---
+++ src/leetcode/FindMaxMatrix.java
@@ -0,0 +1,20 @@
+package leetcode;
+/**
+ * 求最大子矩阵的大小
+ * @author cherish
+ *
+ */
+public class FindMaxMatrix {
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int[][] map = {{1,0,1,1},
+    		          {1,1,1,1},
+    		          {1,1,1,0}};
+	}
+	
+	public static int findMaxMatrix(int[][] map){//我们首先采取时间复杂度为O(m*n)的方法
+		return 0;
+	}
+
+}
