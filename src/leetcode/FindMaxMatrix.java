7a4c4876b90b2d3743eddf2ac17f65e833507e66
WinChuan <huangchuan5@yonyou.com>
2017-07-18 16:32:42 +0800

    找最大矩形面积
    Change-Id: Ic50ce94ee3bae466d777516568d90b919146bc54
    Signed-off-by: WinChuan <huangchuan5@yonyou.com>

--- src/leetcode/FindMaxMatrix.java
+++ src/leetcode/FindMaxMatrix.java
@@ -1,4 +1,7 @@
 package leetcode;
+
+import java.util.Stack;
+
 /**
  * 求最大子矩阵的大小
  * @author cherish
@@ -11,10 +14,30 @@ public class FindMaxMatrix {
        int[][] map = {{1,0,1,1},
     		          {1,1,1,1},
     		          {1,1,1,0}};
+       System.out.println();
 	}
 	
-	public static int findMaxMatrix(int[][] map){//我们首先采取时间复杂度为O(m*n)的方法
-		return 0;
+	public static int maxRecSize(int[][] map){   //我们首先采取时间复杂度为O(m*n)的方法
+		int maxSize = 0;
+		int[] hight = new int[map[0].length];
+		if(map==null||map.length==0||map[0].length==0){
+			return 0;
+		}
+		for(int i = 0 ; i < map.length ; i++){   //用每一列作底去计算矩阵最小的面积
+			for(int j = 0 ;i < map[0].length ; j++){
+				hight[j] = hight[j] == 0 ? 0 : hight[j]+1;
+				maxSize = Math.max(maxSize, maxRecFromBottom(hight));
+			}
+		}
+		return maxSize;
+	}
+	public static int maxRecFromBottom(int[] hight){
+		Stack<Integer> stack = new Stack<Integer>();
+		int maxSize = 0;
+		for(int i = 0 ; i < hight.length ; i++){
+			
+		}
+		return maxSize;
 	}
 
 }
