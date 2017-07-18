a03e5296ea4ddffa74dbe9a879f7b353030514d0
WinChuan <huangchuan5@yonyou.com>
2017-07-18 18:33:11 +0800

    求最大子矩阵的大小
    Change-Id: I8c47c7fefcbae38384069be4c1bdc3ca50d9e199
    Signed-off-by: WinChuan <huangchuan5@yonyou.com>

--- src/leetcode/FindMaxMatrix.java
+++ src/leetcode/FindMaxMatrix.java
@@ -14,7 +14,9 @@ public class FindMaxMatrix {
        int[][] map = {{1,0,1,1},
     		          {1,1,1,1},
     		          {1,1,1,0}};
-       System.out.println();
+      Stack<Integer> stack = new Stack<>();
+      int maxRecSize = maxRecSize(map);
+      System.out.println(maxRecSize);
 	}
 	
 	public static int maxRecSize(int[][] map){   //我们首先采取时间复杂度为O(m*n)的方法
@@ -25,7 +27,7 @@ public class FindMaxMatrix {
 		}
 		for(int i = 0 ; i < map.length ; i++){   //用每一列作底去计算矩阵最小的面积
 			for(int j = 0 ;i < map[0].length ; j++){
-				hight[j] = hight[j] == 0 ? 0 : hight[j]+1;
+				hight[j] = map[i][j] == 0 ? 0 :hight[j]+1;
 				maxSize = Math.max(maxSize, maxRecFromBottom(hight));
 			}
 		}
@@ -35,8 +37,21 @@ public class FindMaxMatrix {
 		Stack<Integer> stack = new Stack<Integer>();
 		int maxSize = 0;
 		for(int i = 0 ; i < hight.length ; i++){
-			
+			while(!stack.isEmpty() && hight[i]<=hight[stack.peek()]){ //向左扩散到k+1,向右扩散到i-1
+				int j = stack.pop();
+				int k = stack.isEmpty() == true? -1 : stack.peek();
+				int area = hight[j]*(i-k-1);
+				maxSize = Math.max(area, maxSize);
+			}
+			stack.push(i);
 		}
+		while(!stack.isEmpty()){                              //还需要清理最后剩在栈中的元素
+			int j = stack.pop();
+			int k = stack.isEmpty() == true? -1 : stack.peek();
+			int area = hight[j]*(hight.length-k-1);           //因为已经可以到达最右边了
+			maxSize = Math.max(area, maxSize);
+		}
+	
 		return maxSize;
 	}
 
