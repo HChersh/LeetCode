59eb1e4ca69cfc4a9185b22e0104460d02dfe71a
ChuanMac <845825101@qq.com>
2017-07-20 08:37:24 +0800

    最大值减去最小值小于等于num的子数组数量
    Change-Id: Ic82be8cb93a1aae4d2da43058f16bcc4cb9d29cd
    Signed-off-by: ChuanMac <845825101@qq.com>

---
+++ src/leetcode/TheNumOfMaxSubMin.java
@@ -0,0 +1,17 @@
+package leetcode;
+/**
+ * 最大值减去最小值小于或等于num的子数组数量
+ * 给定数组arr和整数num,共返回多少个子数组满足如下情况：
+ * max(arr[i..j])-min(arr[i..j] <= num
+ * max(arr[i..j])表示子数组中的最大值，min(arr[i..j])表示子数组中的最小值
+ * @author cherish
+ *
+ */
+public class TheNumOfMaxSubMin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
